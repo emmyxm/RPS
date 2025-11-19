ac54af61e87c69eddbc2292ffd220d26ea81c215
emmyxm <sgtjulietnaboya54321@gmail.com>
2025-11-19 10:23:27 +0100

    last commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function getHumanChoice () {
+    let input = prompt("Please enter your choice");
+    return input.toLowerCase();
+};
+
+function getComputerChoice () {
+    let input = ["rock","paper","scissors"];
+    let randomIndex = Math.floor(Math.random() * input.length);
+    return input[randomIndex];
+};
+
+let humanChoice = 0;
+let computerChoice =0;
+
+function gamePlay(humanChoice, computerChoice) {
+    if (computerChoice === humanChoice){
+        console.log("it a tie");
+    } else if (
+        computerChoice === "rock" && humanChoice === "scissors" ||
+        computerChoice === "scissors" && humanChoice === "paper" ||
+        computerChoice === "paper" && humanChoice === "rock"
+    ){console.log(computerChoice + " you win🎉")
+    } else {console.log("computer wins🎉")};
+};
+
+let human = getHumanChoice();
+let computer = getComputerChoice();
+gamePlay(human, computer);
